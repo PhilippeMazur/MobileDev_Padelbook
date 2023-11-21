7299769e9ff37be3425d1cdf3427ff058771d044
PhilippeMazur <philippe.mazur@student.ap.be>
2023-11-21 17:02:15 +0100

    Add a way to edit preferences

--- app/src/main/java/com/example/padelbook/ui/profile/EditPreferencesFragment.kt
+++ app/src/main/java/com/example/padelbook/ui/profile/EditPreferencesFragment.kt
@@ -1,32 +1,33 @@
 package com.example.padelbook.ui.profile
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.example.padelbook.R
-import com.example.padelbook.models.SharedViewModel
 
-class ActivityFragment : Fragment() {
-    val sharedViewModel: SharedViewModel by activityViewModels()
+class EditPreferencesFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_activity, container, false)
+        return inflater.inflate(R.layout.fragment_editpreferences, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // Add any additional setup code here
+        var button = view.findViewById<TextView>(R.id.savePreferences)
 
-
+        button.setOnClickListener {
+            findNavController().navigateUp()
+        }
 
     }
 }
--- app/src/main/java/com/example/padelbook/ui/profile/PreferencesFragment.kt
+++ app/src/main/java/com/example/padelbook/ui/profile/PreferencesFragment.kt
@@ -1,13 +1,21 @@
 package com.example.padelbook.ui.profile
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.example.padelbook.R
+import com.example.padelbook.models.SharedViewModel
 
 class PreferencesFragment : Fragment() {
+
+    val sharedViewModel: SharedViewModel by activityViewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -21,5 +29,30 @@ class PreferencesFragment : Fragment() {
 
         // Initialize views and set user-specific data
         // ...
+
+
+        var hand = view.findViewById<TextView>(R.id.data_preference_hand)
+        var matchtype = view.findViewById<TextView>(R.id.data_preference_matchtype)
+        var position = view.findViewById<TextView>(R.id.data_preference_position)
+        var time = view.findViewById<TextView>(R.id.data_preference_time)
+        var button = view.findViewById<TextView>(R.id.editPreferences)
+
+        hand.text = sharedViewModel.Preferences.prefered_hand.value
+        matchtype.text = sharedViewModel.Preferences.prefered_match_type.value
+        position.text = sharedViewModel.Preferences.prefered_position.value
+        time.text = sharedViewModel.Preferences.prefered_time.value
+
+
+
+        button.setOnClickListener {
+            val editPreferencesFragment = EditPreferencesFragment()
+            val fragmentManager = activity?.supportFragmentManager
+            val fragmentTransaction = fragmentManager?.beginTransaction()
+            fragmentTransaction?.replace(R.id.fragmentContainerView, editPreferencesFragment)
+            fragmentTransaction?.addToBackStack("editPreferences")
+            fragmentTransaction?.commit()
+        }
+
+
     }
 }
