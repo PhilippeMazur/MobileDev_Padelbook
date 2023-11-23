7b6b2d9978ff05c5a0eab6dec625ac18e1244e9f
samoTh137 <78793115+samoTh137@users.noreply.github.com>
2023-11-23 12:55:31 +0100

    werkt ongeveer?

--- app/src/main/java/com/example/padelbook/ui/profile/ActivityFragment.kt
+++ app/src/main/java/com/example/padelbook/ui/profile/ActivityFragment.kt
@@ -1,18 +1,15 @@
 package com.example.padelbook.ui.profile
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.core.text.HtmlCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.RecyclerView
 import com.example.padelbook.R
-import com.example.padelbook.adapter.MatchAdapter
-import com.example.padelbook.models.Match
 import com.example.padelbook.models.SharedViewModel
 
 class ActivityFragment : Fragment() {
@@ -29,8 +26,13 @@ class ActivityFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
         super.onViewCreated(view, savedInstanceState)
-        val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
-        recyclerView.adapter = MatchAdapter(sharedViewModel.matchList)
+        val linear_layout = view.findViewById<LinearLayout>(R.id.linear_layout)
+        for (item in sharedViewModel.matchList) {
+            val textView1 = TextView(requireContext())
+            textView1.text = item.p1.value.toString()
+
+            linear_layout.addView(textView1)
+        }
 
     }
 
